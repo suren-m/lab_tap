e3bb6c924e738d1c935da5cc77902af799e1ac3c
Suren Mohandass <sumohand@microsoft.com>
2019-11-11 00:38:29 +0000

    01 - Intro to Tasks

---
+++ TapLab/Lab01/Helpers/AsyncHelpers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TapLab.Lab01.Helpers
+{
+    class AsyncHelpers
+    {
+        /// <summary>        
+        /// Should wait Asynchronously for given number of seconds 
+        /// And return the same input passed in for delay         
+        /// Should also accept a cancellation token if the client wishes to cancel
+        /// </summary>
+        /// <returns> int - same value passed in </returns>
+
+        public static async Task<int> DelayedReturnAsync(int seconds, CancellationToken ct = default)
+        {
+            // replace below line with actual implementation
+            return await Task.FromResult(default(int)); 
+        }
+
+
+
+        /// <summary>        
+        /// Prints passed in value if secondsToCancel is less than time taken for DelayedReturnAsync to return the data
+        /// Otherwise, prints TaskCancelled Exception    
+        /// </summary>
+        /// <returns> int - same value passed in </returns>
+
+        public async Task Test_DelayedReturnAsync(int seconds, int secondsToCancel)
+        {
+            try
+            {
+                // replace below line with actual implementation
+                await Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ TapLab/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TapLab.Lab01.Helpers;
+
+namespace TapLab
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Main Started");
+
+            // Uncomment for testing as needed
+
+            //await AsyncHelpers.Test_DelayedReturnAsync(2, 3); // should succeed
+            //await AsyncHelpers.Test_DelayedReturnAsync(2, 1); // should print task cancelled exception
+
+
+
+
+
+
+
+
+            await Task.CompletedTask;
+           
+            Console.WriteLine("\nMain Completed. Press Any Key to Exit\n");
+            Console.ReadLine();
+        }
+
+        
+    }
+}
